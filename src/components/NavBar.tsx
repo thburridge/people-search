3c8bad5570018bfbb68c5cf141bbe065bc12430c
thBurridge <thburridge@gmail.com>
2023-08-23 23:30:53 -0400

    inital

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
         <img
           src={LogoImage}
           alt="Logo"
-          width="70px"
+          width="80px"
           height="auto"
           style={{ cursor: "pointer", marginRight: "10px" }}
         />
