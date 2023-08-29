c0a50c70e1e2a96ffafb1c20c6efa30c223f9897
raynell2 <raynellopez22@hotmail.com>
2023-08-28 22:00:44 -0400

    more commits

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,38 @@
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/people-search", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "people-search",
-  })
-  .then(() => {
-    console.log("Connected to the database");
-  })
-  .catch((err) => {
-    console.error("Error connecting to the database:", err);
-  });
+  .connect("mongodb://127.0.0.1:27017/people-search")
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  lastName: String,
+  email: String,
+});
+
+const userModal = mongoose.model("people", UserSchema, "people"); // Provide "people" as the explicit collection name
+
+app.get("/get", (req, res) => {
+  userModal
+    .find({})
+    .then(function (users) {
+      if (users.length === 0) {
+        res.json({ message: "No users found" });
+      } else {
+        res.json(users);
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+      res.status(500).json({ error: "An error occurred" }); // Sending an error response to the client
+    });
+});
+
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 5050;
+app.listen(port, () => console.log("Listening on port", port));
