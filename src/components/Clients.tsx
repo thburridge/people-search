57ffac6d3c5ed30b673eac7dbdf42b5a7a55b7fc
thBurridge <thburridge@gmail.com>
2023-08-29 17:35:04 -0400

    render table fetch list of customer

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,3 +1,15 @@
+import {
+  Box,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 
 interface User {
@@ -21,15 +33,43 @@ const Clients = () => {
   }, []);
 
   return (
-    <>
-      <h1>User list: </h1>
+    <Box w={"70%"} m={"15px"}>
+      <TableContainer>
+        <Table variant="striped" colorScheme="teal" size="sm">
+          <TableCaption>
+            Customers are coming from a Live Database.
+          </TableCaption>
 
-      <ul>
-        {users.map((user) => (
-          <li key={user._id}>{user.name}</li>
-        ))}
-      </ul>
-    </>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Last name</Th>
+              <Th>Email</Th>
+              <Th>DOB</Th>
+            </Tr>
+          </Thead>
+
+          <Tbody>
+            {users.map((user) => (
+              <Tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.lastname}</td>
+                <td>{user.mobile_email}</td>
+                <td>{user.dob}</td>
+              </Tr>
+            ))}
+          </Tbody>
+
+          <Tfoot>
+            <Tr>
+              <Th>To convert</Th>
+              <Th>into</Th>
+              <Th isNumeric>multiply by</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
 
